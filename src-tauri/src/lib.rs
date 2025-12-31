3ac90f15716831735843039103a4375ae5f680db
langhuihui <178529795@qq.com>
2025-12-31 12:59:01 +0800

    fix: add process plugin for relaunch after update

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -954,6 +954,7 @@ pub fn run() {
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .plugin(
             tauri_plugin_log::Builder::new()
                 .target(tauri_plugin_log::Target::new(
